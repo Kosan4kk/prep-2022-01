b35a5683a4de84fee243eeb071c5c94bfb64f0a1
Paparazzi <48182129+Kosan4kk@users.noreply.github.com>
2022-03-21 01:28:54 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -8,11 +8,11 @@
 #define PROJECT_INCLUDE_UTILS_H_
 
 
-// TODO: Implement `power of` function
+// TODO(Kosan4kk): Implement `power of` function
 // проинициализировать функции
 int custom_pow(int base, int power);
 int isPrime(int n);
 void Factorial(int x);
 size_t timer_from(unsigned char from);
 
-#endif // PROJECT_INCLUDE_UTILS_H_
+#endif  // PROJECT_INCLUDE_UTILS_H_
