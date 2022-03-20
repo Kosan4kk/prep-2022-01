f76118abe8c3a0fe88b0ec37a8788674c8ed8f96
Kosan4kk <kosandeg@gmail.com>
2022-03-20 22:11:05 +0300

    создаем вызовы функций в main

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include "utils.h"
-
+#include "utils.c"
 #define ERR_ARGS_COUNT (-1)
 #define ERR_WRONG_FLG (-2)
 
 #define TST_FOO_FIX     1
 #define TST_FOO_IMPL    2
 #define TST_MOD_IMPL    3
+#define TST_MMM_TRY     4
 
 
 /* NOTE(stitaevskiy):
@@ -13,17 +14,17 @@
  * This function doesn't report conversation errors.
  * For safety program we recommend using `strtol` and its analogs.
  * (See `man atoi` and `man strtol` for more info).
- *
- * const char str_num[] = "1234";
- * char* end = NULL;
- * int val = (int) strtol(str_num, &end, 0);
- * if (end != '\0') {
- *     //ERROR
- * }
- *
- * */
+
+const char str_num[] = "1234";
+char* end = NULL;
+int val = (int) strtol(str_num, &end, 0);
+if (end != '\0') {
+     //ERROR
+}
+*/
 
 int main(int argc, const char** argv) {
+
     if (argc < 3) {
         return ERR_ARGS_COUNT;
     }
@@ -36,26 +37,31 @@ int main(int argc, const char** argv) {
         case TST_FOO_FIX: {
             int to = atoi(data);
             size_t ticks_count = timer_from(to);
-            printf("%d\n", ticks_count);
+            printf("%ld\n", ticks_count);
             break;
         }
         case TST_FOO_IMPL: {
-            if (argc = 4) {
-                // int base = atoi(data);
-                // int pow =  atoi(argv[3]);
-                // int res = custom_pow(base, pow);    // TODO: Implement me
-
-                // printf("%i\n", res);
+            if (argc == 4) {
+                int base = atoi(data);
+                int power = atoi(argv[3]);
+                int res = custom_pow(base, power);    // TODO: Implement me
+                printf("%i\n", res);
+                
             } else {
                 return ERR_ARGS_COUNT;
             }
+            break;
         }
         case TST_MOD_IMPL: {
-            // int num = atoi(data);
-
-            // TODO: Print to stdout `1` if `num` is prime number and `0` otherwise
-            // This function MUST be implemented in
-            // a separate C-module (not in `main` or `utils` module)
+            int num = atoi(data);
+            int res = isPrime(num);
+            printf("%i\n", res);
+            break;
+        }
+        case TST_MMM_TRY: {
+            int num = atoi(data);
+            Factorial(num);
+            break;
         }
         default: {
             return ERR_WRONG_FLG;
