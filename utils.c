86e8e725dfd52e98f16521dae5ee9a0fdd6f2a65
Kosan4kk <kosandeg@gmail.com>
2022-03-20 22:08:23 +0300

    прописываем функции в utils.c

--- utils.c
+++ utils.c
@@ -1,17 +1,40 @@
 #include "utils.h"
 
 size_t timer_from(unsigned char from) {
-    size_t counter;
-    for (unsigned char i = from; i >= 0; --i) {
+    size_t counter = 0;
+    for (unsigned char i = from; i > 0; --i) {
         ++counter;
-        printf(i);
+        printf("%d\n",i);
     }
     return counter;
 }
 
-// TODO: Implement `power of` function
-/*
-int custom_pow(int base, int power) {
-    return 0;
+
+
+int custom_pow(int base, int power) //возвращает результат возведения base в степень pow
+{
+    int a = 0;
+    a = pow(base,power);
+    return a;
 }
-*/
+
+int isPrime(int n) //является ли число, поданное на вход, простым
+{
+    if (n > 1)
+    {
+        for (int i = 2; i < n; i++){
+            if (n % i == 0){
+                return 0;
+            }
+        }
+        return 1;
+    }
+    else
+        return 0;
+}
+
+void Factorial(int x) //выводит через пробел все целые числа от 1 до n
+{
+    if (x > 1) F(x-1);
+    printf("%d ",x);
+}
