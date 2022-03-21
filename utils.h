0bd539f56a830324477a4188d0ffcef5e46abd0e
Paparazzi <48182129+Kosan4kk@users.noreply.github.com>
2022-03-21 09:14:38 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -10,8 +10,8 @@
 
 
 // TODO(Kosan4kk): Implement `power of` function
-/*// проинициализировать функции
+// проинициализировать функции
 int custom_power(int base, int power);
 int isPrime(int n);
 void Factorial(int x);
-size_t timer_from(unsigned char from);*/
+size_t timer_from(unsigned char from);
