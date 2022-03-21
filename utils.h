1e4ea1c90d19cd6d083568841781522ca472a277
Paparazzi <48182129+Kosan4kk@users.noreply.github.com>
2022-03-21 09:29:48 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,6 +1,12 @@
 #ifndef PROJECT_INCLUDE_UTILS_H_
 #define PROJECT_INCLUDE_UTILS_H_
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+// добавляем все нужные библиотеки
+
 // TODO(Kosan4kk): Implement `power of` function
 // проинициализировать функции
 int custom_power(int base, int power);
